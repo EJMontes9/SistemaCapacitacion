dfe2708e411e0f7a9bf049f5f211bd494c4a09d6
Erick Montes <erickmontes77.jm@gmail.com>
2023-12-11 06:30:20 -0500

    Maquetado del dashboard

---
+++ resources/views/components/progreso-cursos.blade.php
@@ -0,0 +1,53 @@
+<h3 class="text-xl leading-none font-bold text-gray-900 mb-10">Progreso de cursos</h3>
+<div class="block w-full overflow-x-auto">
+    <table class="items-center w-full bg-transparent border-collapse">
+        <thead>
+        <tr>
+            <th class="px-4 bg-gray-50 text-gray-700 align-middle py-3 text-xs font-semibold text-left uppercase border-l-0 border-r-0 whitespace-nowrap">Cursos</th>
+            <th class="px-4 bg-gray-50 text-gray-700 align-middle py-3 text-xs font-semibold text-left uppercase border-l-0 border-r-0 whitespace-nowrap min-w-140-px">Progreso</th>
+        </tr>
+        </thead>
+        <tbody class="divide-y divide-gray-100">
+        <tr class="text-gray-500">
+            <th class="border-t-0 px-4 align-middle text-sm font-normal whitespace-nowrap p-4 text-left">Angular</th>
+            <td class="border-t-0 px-4 align-middle text-xs whitespace-nowrap p-4">
+                <div class="flex items-center">
+                    <span class="mr-2 text-xs font-medium">30%</span>
+                    <div class="relative w-full">
+                        <div class="w-full bg-gray-200 rounded-sm h-2">
+                            <div class="bg-cyan-600 h-2 rounded-sm" style="width: 30%"></div>
+                        </div>
+                    </div>
+                </div>
+            </td>
+        </tr>
+        <tr class="text-gray-500">
+            <th class="border-t-0 px-4 align-middle text-sm font-normal whitespace-nowrap p-4 text-left">Laravel</th>
+            <td class="border-t-0 px-4 align-middle text-xs whitespace-nowrap p-4">
+                <div class="flex items-center">
+                    <span class="mr-2 text-xs font-medium">70%</span>
+                    <div class="relative w-full">
+                        <div class="w-full bg-gray-200 rounded-sm h-2">
+                            <div class="bg-fuchsia-600 h-2 rounded-sm" style="width: 70%"></div>
+                        </div>
+                    </div>
+                </div>
+            </td>
+        </tr>
+        <tr class="text-gray-500">
+            <th class="border-t-0 px-4 align-middle text-sm font-normal whitespace-nowrap p-4 text-left">Springboot</th>
+            <td class="border-t-0 px-4 align-middle text-xs whitespace-nowrap p-4">
+                <div class="flex items-center">
+                    <span class="mr-2 text-xs font-medium">45%</span>
+                    <div class="relative w-full">
+                        <div class="w-full bg-gray-200 rounded-sm h-2">
+                            <div class="bg-indigo-600 h-2 rounded-sm" style="width: 45%"></div>
+                        </div>
+                    </div>
+                </div>
+            </td>
+        </tr>
+
+        </tbody>
+    </table>
+</div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,58 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
+    <nav class="bg-white border-b border-gray-200 fixed z-30 w-full">
+        <div class="px-3 py-3 lg:px-5 lg:pl-3">
+            <div class="flex items-center justify-between">
+                <div class="flex items-center justify-start">
+                    <button id="toggleSidebarMobile" aria-expanded="true" aria-controls="sidebar" class="lg:hidden mr-2 text-gray-600 hover:text-gray-900 cursor-pointer p-2 hover:bg-gray-100 focus:bg-gray-100 focus:ring-2 focus:ring-gray-100 rounded">
+                        <svg id="toggleSidebarMobileHamburger" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
+                        </svg>
+                        <svg id="toggleSidebarMobileClose" class="w-6 h-6 hidden" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                        </svg>
+                    </button>
+                    <a href="#" class="text-xl font-bold flex items-center lg:ml-2.5 w-1/3 h-1/3">
+                        <x-application-mark class="block h-9 w-auto" />
+                        <span class="self-center whitespace-nowrap ml-3">StudyApp</span>
+                    </a>
+                    <form action="#" method="GET" class="hidden lg:block lg:pl-32">
+                        <label for="topbar-search" class="sr-only">Search</label>
+                        <div class="mt-1 relative lg:w-64">
+                            <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                                <svg class="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path>
+                                </svg>
+                            </div>
+                            <input type="text" name="email" id="topbar-search" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full pl-10 p-2.5" placeholder="Search">
+                        </div>
+                    </form>
+                </div>
+                <div class="flex items-center">
+                    <button id="toggleSidebarMobileSearch" type="button" class="lg:hidden text-gray-500 hover:text-gray-900 hover:bg-gray-100 p-2 rounded-lg">
+                        <span class="sr-only">Search</span>
+                        <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path>
+                        </svg>
+                    </button>
+                </div>
             </div>
         </div>
+    </nav>
+
+
+    <div>
+
+        <div class="flex overflow-hidden bg-white pt-16">
+            <div class="bg-gray-900 opacity-50 hidden fixed inset-0 z-10" id="sidebarBackdrop"></div>
+            <div id="main-content" class="h-full w-full bg-gray-50 relative overflow-y-auto lg:ml-64">
+                <x-welcome />
+                <x-footer></x-footer>
+            </div>
+        </div>
+        <script async defer src="https://buttons.github.io/buttons.js"></script>
+        <script src="https://demo.themesberg.com/windster/app.bundle.js"></script>
     </div>
+
+
+
 </x-app-layout>
