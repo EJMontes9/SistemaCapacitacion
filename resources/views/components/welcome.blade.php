747956226772836dbaaf5c9d0c6b2800a1a17d75
Erick Montes <erickmontes77.jm@gmail.com>
2023-12-11 02:48:29 -0500

    Auto stash before merge of "InterfazPrincipal" and "origin/InterfazPrincipal"

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -5,12 +5,13 @@
         Bienvenidos a la mejor aplicacion educativa
     </h1>
 
-    <p class="mt-6 text-gray-500 leading-relaxed">
+    <!--<p class="mt-6 text-gray-500 leading-relaxed">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec volutpat justo, pellentesque bibendum purus.
         Phasellus rhoncus sagittis euismod. Vivamus quis aliquam purus. Nunc eu lorem et nisl rutrum eleifend.
         Cras imperdiet tincidunt metus sed suscipit. Pellentesque non elementum libero. Ut tempus augue nec est consectetur,
         vel consequat ex pharetra. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
         Ut ipsum est, aliquam ac tempor ac, aliquet et velit. Mauris commodo eget nunc in laoreet.
-    </p>
+    </p>-->
+    <x-graficos></x-graficos>
 </div>
 
