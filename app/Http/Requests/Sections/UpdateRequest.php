14a63f1615574a42c3a3a541270fb5f5d6f402cd
Erick Montes <erickmontes77.jm@gmail.com>
2024-01-26 06:58:19 -0500

    Mejoras en seccion

---
+++ app/Http/Requests/Sections/UpdateRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests\Sections;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|min:5|max:255',
+            'course_id' => 'required|integer',
+        ];
+    }
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+}
--- resources/views/components/sections/form-sections.blade.php
+++ resources/views/components/sections/form-sections.blade.php
@@ -1,6 +1,6 @@
 <div class="flex flex-col justify-center">
     <div class="flex flex-col w-full lg:flex-row">
-        <x-input class="mt-4 lg:mr-2 lg:w-1/2" type="text" name="name" placeholder="Titulo de la seccion" value="{{ old('title', $sections->title ?? '') }}" />
+        <x-input class="mt-4 lg:mr-2 lg:w-1/2" type="text" name="name" placeholder="Titulo de la seccion" value="{{ old('title', $sections->name ?? '') }}" />
         <x-combobox class="mt-4 lg:w-1/2 lg:mr-2 text-gray-500" name="course_id">
             <x-slot name="title">Curso</x-slot>
             <option value="0">Seleccione un curso...</option>
